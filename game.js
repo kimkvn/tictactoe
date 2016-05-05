b165fd687f48d7b417e72fc408f9ef99c4dd951d
kimkvn <kimkvn@gmail.com>
2016-05-05 18:37:10 -0400

    copied in my work from jsfiddle, gave each square a number for my sanity

---
+++ game.js
@@ -0,0 +1,14 @@
+//conditions for winning
+// 1-2-3, 4-5-6, 7-8-9
+// 1-4-7, 2-5-8, 3-6-9
+// 1-5-9, 3-5-7
+
+$('.square').click(function(){
+  var $result = $(this).attr('class');
+  //alert($result);
+  $(this).toggleClass('filled');
+
+  var x = $('.square.filled')
+	console.log(x)
+
+});
