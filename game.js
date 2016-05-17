e47f3129d641698573e124f74c14687562101f24
kimkvn <kimkvn@gmail.com>
2016-05-17 19:34:43 -0400

    man games are hard. rulez. fuck da rulez

--- game.js
+++ game.js
@@ -2,22 +2,34 @@
 // 1-2-3, 4-5-6, 7-8-9
 // 1-4-7, 2-5-8, 3-6-9
 // 1-5-9, 3-5-7
+var blues = [];
 
 $('.square').click(function(){
   var $result = $(this).attr('class');
   //alert($result);
+  blues.push($(this).attr('id'));
   $(this).toggleClass('filledBlue');
+
+  console.log(blues);
 
   counter();
 
 });
+var tally = function(){
+  var blues = [];
+
+};
 
 var counter = function(){
   if(
     ($('.one').hasClass('filledBlue') && $('.two').hasClass('filledBlue') && $('.three').hasClass('filledBlue'))
     ||
     ($('.four').hasClass('filledBlue') && $('.five').hasClass('filledBlue') && $('.six').hasClass('filledBlue'))
-  ){
+  )
+  var arr = $('.filledBlue');
+  for(i = 0; i < arr.length; i++)
+  {
+    if(arr[i] )
     alert('A WINNER IS YOU!')
   };
 };
