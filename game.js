3901817a929142b40d64eadcbfe2c6ea20f70b2a
kimkvn <kimkvn@gmail.com>
2016-05-25 13:37:58 -0400

    created a reset game function

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ $('.square').click(function(){
   var $result = $(this).attr('class');
   //alert($result);
   blues.push($(this).attr('id'));
+  $(this).click(false);
   $(this).toggleClass('filledBlue');
 
   console.log(blues);
@@ -25,11 +26,19 @@ var counter = function(){
     ($('.one').hasClass('filledBlue') && $('.two').hasClass('filledBlue') && $('.three').hasClass('filledBlue'))
     ||
     ($('.four').hasClass('filledBlue') && $('.five').hasClass('filledBlue') && $('.six').hasClass('filledBlue'))
-  )
-  var arr = $('.filledBlue');
-  for(i = 0; i < arr.length; i++)
-  {
-    if(arr[i] )
-    alert('A WINNER IS YOU!')
-  };
+  ){
+    alert('A WINNER IS YOU!');
+    reset();
+  }
+  // var arr = $('.filledBlue');
+  // for(i = 0; i < arr.length; i++)
+  // {
+  //   if(arr[i] )
+  //   alert('A WINNER IS YOU!')
+  // };
+};
+
+var reset = function(){
+  $('.square').removeClass('filledBlue');
+  blues = [];
 };
